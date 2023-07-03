654f12c9701395a1ec62a665b6980a07a124555a
17TheWord <54731914+17TheWord@users.noreply.github.com>
2023-07-03 04:05:39 +0000

    Deploying to gh-pages from  @ a6b30eee7aca905c5a6fbf34ab5be4d60dbd4771 🚀

---
+++ assets/index.html.66f15613.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-7449e9bf","path":"/mgc/","title":"\u5BFC\u8A00","lang":"zh-CN","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1688357108000,"contributors":[{"name":"17TheWord","email":"54731914+17TheWord@users.noreply.github.com","commits":1}]},"filePathRelative":"mgc/README.md"}');export{e as data};
--- assets/mod.html.4f090350.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-adf5648e","path":"/mchelp/resource/mod.html","title":"Mod\u4E0B\u8F7D\u533A","lang":"zh-CN","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1682247678000,"contributors":[{"name":"17TheWord","email":"17theword@gmail.com","commits":1}]},"filePathRelative":"mchelp/resource/mod.md"}');export{e as data};
